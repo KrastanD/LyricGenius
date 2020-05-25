f8a1ee98dd83dfb5075e80424616a34c2aaaba24
Krastan D <kdimit@illinois.edu>
2020-05-25 18:09:21 -0500

    get tokens was renamed to match its function it was also changed to use the correct way to get time and set the expiration time

---
+++ getTokens.js
@@ -0,0 +1,36 @@
+import { encode as btoa } from "base-64";
+import getAuthorizationCode from "./getAuthorizationCode";
+import spotifyCredentials from "./secrets";
+import setUserData from "./storage/setUserData";
+
+export default getTokens = async () => {
+  console.log("getTokens");
+  try {
+    const authorizationCode = await getAuthorizationCode();
+    const credsB64 = btoa(
+      `${spotifyCredentials.CLIENT_ID}:${spotifyCredentials.CLIENT_SECRET}`
+    );
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${credsB64}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${spotifyCredentials.REDIRECT_URI}`,
+    });
+
+    const responseJson = await response.json();
+    const {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expires_in: expiresIn,
+    } = responseJson;
+
+    const expirationTime = Date.now() + expiresIn * 1000;
+    await setUserData("accessToken", accessToken);
+    await setUserData("refreshToken", refreshToken);
+    await setUserData("expirationTime", expirationTime.toString());
+  } catch (err) {
+    console.error(err);
+  }
+};
